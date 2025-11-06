af06f9602b38b4a760b965e702a992c13718abda
Moises Velasquez <moises007@gmail.com>
2025-11-06 17:36:15 -0500

    pagintation

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -3,9 +3,22 @@ import { NextResponse, NextRequest } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request) {
-  // const todos = await prisma.todo.findMany();
-  // return NextResponse.json(todos);
-  const todos = await prisma.todo.findMany();
-  return Response.json(todos);
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  // const { searchParams } = new URL(request.url);
+  const take = Number(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+
+  if (isNaN(take)) {
+    return NextResponse.json({ message: "Take must be a number" }, { status: 400 });
+  }
+  if (isNaN(skip)) {
+    return NextResponse.json({ message: "Take must be a number" }, { status: 400 });
+  }
+
+  const todos = await prisma.todo.findMany({
+    take,
+    skip,
+  });
+  return NextResponse.json(todos);
 }
