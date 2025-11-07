2d5350c536095ccc1b35f21cb860b7d6209b21d3
Moises Velasquez <moises007@gmail.com>
2025-11-06 19:35:19 -0500

    UPDATE TODO

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -20,3 +20,24 @@ export async function GET(request: Request, segments: Segments) {
 
   return NextResponse.json(todo);
 }
+
+export async function PUT(request: Request, segments: Segments) {
+  const { id } = await segments.params;
+
+  const todo = await prisma.todo.findFirst({
+    where: { id },
+  });
+
+  if (!todo) {
+    return NextResponse.json({ messsage: `Todo with id: ${id} not found` }, { status: 404 });
+  }
+
+  const body = await request.json();
+
+  const updateTodo = await prisma.todo.update({
+    where: { id },
+    data: body,
+  });
+
+  return NextResponse.json(updateTodo);
+}
