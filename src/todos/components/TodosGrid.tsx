976e72acc77f2193a6177605d7a1870cc92c2378
Moises Velasquez <moises007@gmail.com>
2025-11-25 22:54:46 -0500

    update server component

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -4,17 +4,24 @@ import { Todo } from "@/generated/prisma";
 import { TodoItem } from "./TodoItem";
 
 import * as api from "@/todos/helpers/todos.helper";
+import { useRouter } from "next/navigation";
 
 interface ITodosGrid {
   todos?: Todo[];
 }
 
 export const TodosGrid = ({ todos = [] }: ITodosGrid) => {
-  console.log(todos);
+  const router = useRouter();
+
+  const toggleTodo = async (id: string, complete: boolean) => {
+    const updatedTodo = await api.updateTodo(id, complete);
+    console.log({ updatedTodo });
+    router.refresh();
+  };
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
       {todos.map((todo) => (
-        <TodoItem todo={todo} toggleTodo={api.updateTodo} key={todo.id} />
+        <TodoItem todo={todo} toggleTodo={toggleTodo} key={todo.id} />
       ))}
     </div>
   );
