1d56c81346d458118778733de70d26a597ae4c82
Moises Velasquez <moises007@gmail.com>
2026-02-16 23:44:31 -0500

    chore(top-menu): fix render

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -14,9 +14,9 @@ export const TopMenu = () => {
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
@@ -74,9 +74,9 @@ export const TopMenu = () => {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
               />
             </svg>
