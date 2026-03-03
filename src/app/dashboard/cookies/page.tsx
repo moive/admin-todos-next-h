da6115485d186df341d2b8848ce57dadfe22c117
Moises Velasquez <moises007@gmail.com>
2026-03-03 18:08:06 -0500

    feat(cookie): implement cookie, client and server

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,15 +1,20 @@
+import { cookies } from 'next/headers';
 import { TabBar } from '../../../components/TabBar';
+
 export const metadata = {
   title: 'Cookies Page',
   description: 'Cookies Page',
 };
 
-export default function CookiesPage() {
+export default async function CookiesPage() {
+  const cookieStore = await cookies();
+  const cookieSelectedTab = cookieStore.get('selectedTab')?.value ?? '1';
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentIndex={+cookieSelectedTab} />
       </div>
     </div>
   );
--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { setCookie } from 'cookies-next';
 import { useState } from 'react';
 
 // https://tailwindcomponents.com/component/radio-buttons-1
@@ -13,6 +14,7 @@ export const TabBar = ({ tabOptions = [1, 2, 3, 4], currentIndex }: Props) => {
   const [selected, setSelected] = useState(currentIndex);
   const onTabSelected = (tab: number) => {
     setSelected(tab);
+    setCookie('selectedTab', tab.toString());
   };
 
   return (
